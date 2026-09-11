6592583b73b4eb75a78e60f733b2dc3f7aa4422d
guroveGames <iconbit301216@gmail.com>
2026-09-11 16:31:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw, ImageFont
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 # Скачиваем шрифт Anton (Impact-подобный, с кириллицей) при старте
-FONT_URL = "https://github.com/google/fonts/raw/main/ofl/anton/Anton-Regular.ttf"
+FONT_URL = "https://github.com/googlefonts/OswaldFont/raw/main/fonts/static_ttf/Oswald-Bold.ttf"
 FONT_PATH = "impact.ttf"
 if not os.path.exists(FONT_PATH):
     urllib.request.urlretrieve(FONT_URL, FONT_PATH)
